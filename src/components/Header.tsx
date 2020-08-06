04d6014387d875d9c1032b2440507fefb1ccf4f3
Stefano Casasola <stepanello@gmail.com>
2020-08-06 09:55:25 +0200

    Finished the project

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,30 +1,31 @@
-import React from 'react'
+import React from "react";
 
 interface HeaderProps {
-  title: string
-  subTitle?: string
+  title: string;
+  subTitle?: string;
 }
 
 interface HeaderState {
-  count: number
+  count: number;
 }
 
 class Header extends React.Component<HeaderProps, HeaderState> {
   state: HeaderState = {
     count: 0,
-  }
+  };
 
-  increaseState = () => this.setState({ count: this.state.count + 1 })
+  increaseState = (): void => this.setState({ count: this.state.count + 1 });
 
   render() {
     return (
       <>
-        <h1 onClick={this.increaseState}>{this.props.title}</h1>
-        {this.props.subTitle ? <h2>{this.props.subTitle}</h2> : <div></div>}
-        <div>{this.state.count}</div>
+        <h1 onClick={this.increaseState}>
+          {this.props.title}, value in the state: {this.state.count}
+        </h1>
+        {this.props.subTitle ? <h2>{this.props.subTitle}</h2> : <></>}
       </>
-    )
+    );
   }
 }
 
-export default Header
+export default Header;
